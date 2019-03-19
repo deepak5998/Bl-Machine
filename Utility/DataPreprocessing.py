65e983d93e9f382d3d7cc58e48bbc08f50f553de
prayas2409 <prayas.hemraj.pagade@gmail.com>
2019-03-19 19:38:28 +0530

    Class to implement Data preprocessing functions

---
+++ Utility/DataPreprocessing.py
@@ -0,0 +1,33 @@
+import Pandas as pd
+import numpy as np
+
+class DataPreprocessing:
+
+    def __init__(self):
+        print()
+
+    @staticmethod
+    def complete_Replace(dataframe, original, new):
+        return dataframe[original].replace(original, new, inplace=True)
+
+    @staticmethod
+    def one_hot_encoder(dataframe):
+        return pd.get_dummies(dataframe)
+
+    @staticmethod
+    def fill_Na(dataframe, column, fill):
+        dataframe[column] = dataframe[column].fillna(fill)
+        return dataframe
+
+    @staticmethod
+    def feature_scaling(dataframe, column):
+        dataframe[column] = np.divide(np.subtract(dataframe, dataframe[column].mean()),
+                                      dataframe[column].std())
+        return dataframe[column]
+
+    @staticmethod
+    def split(dataframe, numrows):
+        test_data = dataframe.tail(numrows).reset_index(drop=True)
+        # here we reset index as if the index remain same for accessing by index could be difficult
+        return dataframe.head(len(dataframe)-numrows).reset_index(), test_data.reset_index()
+
