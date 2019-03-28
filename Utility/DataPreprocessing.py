e540959a6714c485fe5b3fd4d7be6b5c903fdfb4
prayas2409 <prayas.hemraj.pagade@gmail.com>
2019-03-28 19:54:14 +0530

    Gradient Descent

--- Utility/DataPreprocessing.py
+++ Utility/DataPreprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 class DataPreprocessing:
 
@@ -21,15 +22,24 @@ class DataPreprocessing:
 
     @staticmethod
     def feature_scaling(dataframe, column):
-        dataframe[column] = np.divide(np.subtract(dataframe, dataframe[column].mean()),
-                                      dataframe[column].std())
+        dataframe[column] = np.divide(np.subtract(dataframe, dataframe[column].min()),
+                                      np.subtract(dataframe[column].max(),dataframe[column].min()))
         return dataframe[column]
 
     @staticmethod
     def split(dataframe, numrows):
-        test_data = dataframe.tail(numrows).reset_index(drop=True)
+        test_data = dataframe.tail(numrows).reset_index(drop=True).reset_index()
         # here we reset index as if the index remain same for accessing by index could be difficult
-        return dataframe.head(len(dataframe)-numrows).reset_index(), test_data.reset_index()
+        dataframe = dataframe.head(dataframe.shape[0]-numrows).reset_index()
+        try:
+            test_data.drop(['index'],axis=1)
+        except Exception as e:
+            print()
+        try: 
+            dataframe.drop(['index'],axis=1)
+        except Exception as e:
+            print()
+        return dataframe, test_data
 
     @staticmethod
     def sigmoid_Function(Z):
@@ -39,3 +49,26 @@ class DataPreprocessing:
     @staticmethod
     def cube(x):
         print(x**3)
+    
+    @staticmethod
+    def separate(dataframe,col):
+        output = dataframe[col]
+        return dataframe.drop(col,axis=1),output
+    
+    @staticmethod
+    def remove_outlier(dataframe):
+        dataframe = dataframe[np.abs(dataframe - dataframe.mean()) <= (3 * dataframe.std())]
+        try:
+            dataframe.drop('index')
+        except Exception as e:
+            pass
+        return dataframe.reset_index()
+    
+    @staticmethod
+    def main(dataframe):
+        dataframe.hist(figsize=(10,10))
+        plt.show()
+        print(dataframe.columns,"\nPlease copy paste and enter the columns to be scaled")
+        col = input().split(',')
+        print(col)
+        print(list(col))
